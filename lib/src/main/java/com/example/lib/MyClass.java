3948994a72f5dfa0114c006abe2cbafc2e29cb38
cyk90290 <57485322+cyk90290@users.noreply.github.com>
2020-01-03 14:59:39 +0800

    Update MyClass.java

--- lib/src/main/java/com/example/lib/MyClass.java
+++ lib/src/main/java/com/example/lib/MyClass.java
@@ -1,35 +1,46 @@
 package com.example.lib;
 import java.util.Scanner;
 public class MyClass {
+Scanner scanner = new Scanner(System.in);
+    private int Customer[];
+    private int TransactionTime;
 
-    public static void main(String[] args) {
-        int Year=0,Month=0,Day=0,TotalDay=0;
-        int MonthDay[]={31,28,31,30,31,30,31,31,30,31,30,31};
-        boolean LeapYear=false;
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("請輸入西元年 月 日");
-        Year = scanner.nextInt();
-        Month = scanner.nextInt();
-        Day = scanner.nextInt();
-        if(Year%4!=0)
-            LeapYear=false;
-        else if(Year%100!=0)
-            LeapYear=true;
-        else if(Year%400!=0)
-            LeapYear=false;
-        else
-            LeapYear=true;
-        if(LeapYear==true)
-            MonthDay[1]=29;
-        if(Month>12)
-            System.out.println("請輸入正確月份!");
-        else if(MonthDay[Month-1]<Day)
-            System.out.printf("輸入錯誤!!%d年的%d月只有%d天\n",Year,Month,MonthDay[Month-1]);
-        else {
-            for(int i=Month-2;i>=0;--i){
-                TotalDay+=MonthDay[i];
+    public CppBank(int CustomerN, int TransactionTimeN) {
+        while (CustomerN > 100) {
+            System.out.println("不能超過100個顧客，請從新輸入");
+            CustomerN = Integer.parseInt(scanner.next());
+        }
+        while (TransactionTimeN > 10000) {
+            System.out.println("不能超過10000個交易次數，請從新輸入");
+            TransactionTimeN = Integer.parseInt(scanner.next());
+        }
+        Customer = new int[CustomerN];
+        TransactionTime = TransactionTimeN;
+        for (int i = 0; i < CustomerN; i++) {               //初始化
+            Customer[i] = 0;
+        }
+        for(int i=0;i<TransactionTime;i++){                 //交易開始
+            int ThisTimeCustomer = Integer.parseInt(scanner.next());
+            while (ThisTimeCustomer>CustomerN){
+                System.out.println("超過原輸入的顧客數，請再次輸入");
+                ThisTimeCustomer = Integer.parseInt(scanner.next());
             }
-            TotalDay+=Day;
-            System.out.printf("這天是今年第%d天\n",TotalDay);
+            int Form_of_transaction = Integer.parseInt(scanner.next());
+            while (Form_of_transaction!=1&&Form_of_transaction!=2){
+                System.out.println("請再次輸入，存款為1、提款為2");
+                Form_of_transaction = Integer.parseInt(scanner.next());
+            }
+            int ThisTimeTransactionM = Integer.parseInt(scanner.next());
+            switch (Form_of_transaction){
+                case 1:
+                    Customer[ThisTimeCustomer] += ThisTimeTransactionM;
+                    break;
+                case 2:
+                    Customer[ThisTimeCustomer] -= ThisTimeTransactionM;
+                    break;
+            }
+        }
+        for (int i = 0; i < CustomerN; i++){
+            System.out.printf("%d : %d \n",i,Customer[i]);
         }
     }}
